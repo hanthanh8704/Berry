9549453ce0e7812dc25092ca5bcdb5ce034dcfeb
hanthanh8704 <thanhhhph40692@fpt.edu.vn>
2024-06-21 16:04:00 +0700

    Code hom nay DUc

---
+++ fe/src/menu-items/index.js
@@ -0,0 +1,16 @@
+import pages from './pages';
+import dashboard from './dashboard';
+import product from './product';
+import voucher from './voucher';
+import account from './account';
+import bill from './bill';
+import orders from './orders';
+import statistical from './statistical';
+
+// ==============================|| MENU ITEMS ||============================== //
+
+const menuItems = {
+  items: [pages,dashboard,orders,product,bill,voucher,account,statistical]
+};
+
+export default menuItems;
