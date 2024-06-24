af9ec7e8ecf876e76532b4cda7ccbf1ab6836baf
Datdt0110 <datt2303@gmai.com>
2024-06-24 14:36:36 +0700

    thanhdat

--- be/src/main/java/com/example/connectdb/assignment1/BanHangRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.connectdb.assignment1;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class BanHangRepository {
-
-}
---
+++ be/src/main/java/com/example/connectdb/controller/SanPhamControllers.java
@@ -0,0 +1,8 @@
+package com.example.connectdb.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SanPhamControllers {
+    
+}
